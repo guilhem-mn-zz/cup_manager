f49ec6ff70020d17ca9fafcb2b6ad07ad2ebef35
Poupipomme <poupipomme@gmail.com>
2018-11-04 12:44:05 +0100

    .env

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+require('dotenv').config();
+//TODO:
+//react handle
+//json
+//tweet
+
+
+const { readFileSync } = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -10,6 +18,8 @@ class Cup {
         this.lien = "https://discord.gg/"+lien;
     }
 };
+
+
 
 //Prefix
 const prefix = "!";
@@ -29,8 +39,6 @@ const cup = "**"+prefix+"cup**: Display complete list of the upcomming events, b
 
 embed.setDescription(h+n+addcup+n+argcup+n+cup);
 
-
-
 //Game
 gamel = [
     prefix+`h for Help`,
@@ -38,9 +46,14 @@ gamel = [
     `Made by Gui#5440`
 ]
 
-function randomG(min, max) {
+function randomG (min, max) {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
+function randomC () {
+    return Math.random().toString(16).slice(2, 8).toUpperCase();
+}
+
+let color = randomC();
 
 function game() {
     //Loop toutes les 30s
@@ -59,6 +72,8 @@ function game() {
             setTimeout(game, 30000);
         }
 }
+//Login
+client.login(process.env.TOKEN);
 
 //Connected
 client.on('ready', () => {
@@ -66,12 +81,6 @@ client.on('ready', () => {
   setTimeout(game, 1);
 });
 
-//gestion des reactions
-/*client.on('messageReactionAdd', function(reaction) {
-    let users = reaction.users;
-    console.log(users);
-
-});*/
 
 client.on('message', msg => {
 
@@ -91,7 +100,7 @@ client.on('message', msg => {
         //Cup property
         cupe.setAuthor(macup.nom);
         cupe.setColor('NAVY');
-        cupe.setDescription(macup.date+'\n\n'+macup.map+'\n\n'+macup.lien+'\n\n');
+        cupe.setDescription(macup.date+'\n\n'+macup.map+'\n\n'+macup.lien+'\n'+'Make sure to react with ✅ to the message register yourself to the cup');
 
         //Sending cup + react
         msg.channel.send(cupe)
@@ -100,16 +109,29 @@ client.on('message', msg => {
             message.react("❌")
         });
         
-        //creation des roles avec le nom de la cup        
+        //creation des roles avec le nom de la cup         
+        //FAIRE QUE LE ROLE SOIT TAGGABLE + couleur aleatoire
         msg.guild.createRole({
             name: macup.nom,
-            permissions: []
-        }).then ((role) => {
-            //donation des roles aux react
-            msg.member.addRole(role.id);
-        }).catch ((e) => console.log("err: "+e));
-        
-      //ajout de l'objet en bdd ?
+            color: color,
+            permissions: [],
+            mentionable: true
+        });
+
+        if (fs.exists) {
+
+            
+
+        //ajout de l'objet en json ?
+        let json = {
+        "nom": macup.name,
+        "date": macup.date,
+        "map": macup.map,
+        "lien": macup.lien
+        };
+
+        fs.writeFileSync("json.txt", json, "json");
+        }  
 
     }
 
@@ -119,17 +141,10 @@ client.on('message', msg => {
             msg.channel.send(embed);
         break;
 
-        case 'r':
-            msg.react("✅");
-        break;
-
         case prefix+'cup':
             console.log('COMMANDE: cup');
             msg.channel.send("Recents Cup");
-            //requete
+
         break;
     }
 });
-
-
-client.login('');
